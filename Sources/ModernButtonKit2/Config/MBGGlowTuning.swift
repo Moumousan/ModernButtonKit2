b47ab1efc3faeeaa8b9a476f828126b6f03cedb5
Mou <29241499+Moumousan@users.noreply.github.com>
2026-01-26 08:35:17 +0900

    MBGGlowTuningを修正。引数を取れるようにした。イニシャライザを孵化してある。

--- Sources/ModernButtonKit2/Config/MBGGlowTuning.swift
+++ Sources/ModernButtonKit2/Config/MBGGlowTuning.swift
@@ -8,38 +8,48 @@
 import SwiftUI
 
 
-// Kit2 内（Core か Config フォルダ）
-public struct MBGGlowTuning: Sendable {
-    // 各プリセットの内側・外側の不透明度
-    public var fillSubtle: Double
-    public var fillNormal: Double
-    public var fillStrong: Double
-
+public struct MBGGlowTuning: Equatable, Sendable {
+    public var fillSubtle:  Double
     public var strokeSubtle: Double
+    public var fillNormal:  Double
     public var strokeNormal: Double
+    public var fillStrong:  Double
     public var strokeStrong: Double
 
-    /// cornerRadius に掛けるベースのぼかし係数
-    public var baseBlurScale: CGFloat
+    public var baseBlurScale: CGFloat   // cornerRadius から blur を決める係数
+    public var spreadTight:   CGFloat
+    public var spreadMedium:  CGFloat
+    public var spreadWide:    CGFloat
 
+    // ← ここに public init を書く
     public init(
-        fillSubtle: Double,
-        fillNormal: Double,
-        fillStrong: Double,
-        strokeSubtle: Double,
-        strokeNormal: Double,
-        strokeStrong: Double,
-        baseBlurScale: CGFloat
-    ) {
-        self.fillSubtle = fillSubtle
-        self.fillNormal = fillNormal
-        self.fillStrong = fillStrong
-        self.strokeSubtle = strokeSubtle
-        self.strokeNormal = strokeNormal
-        self.strokeStrong = strokeStrong
-        self.baseBlurScale = baseBlurScale
-    }
+           fillSubtle:  Double = 0.20,
+           strokeSubtle: Double = 0.40,
+           fillNormal:  Double = 0.45,
+           strokeNormal: Double = 0.90,
+           fillStrong:  Double = 0.70,
+           strokeStrong: Double = 1.00,
+           baseBlurScale: CGFloat = 0.60,
+           spreadTight:   CGFloat = 0.60,
+           spreadMedium:  CGFloat = 1.00,
+           spreadWide:    CGFloat = 1.40
+       ) {
+           self.fillSubtle    = fillSubtle
+           self.strokeSubtle  = strokeSubtle
+           self.fillNormal    = fillNormal
+           self.strokeNormal  = strokeNormal
+           self.fillStrong    = fillStrong
+           self.strokeStrong  = strokeStrong
+           self.baseBlurScale = baseBlurScale
+           self.spreadTight   = spreadTight
+           self.spreadMedium  = spreadMedium
+           self.spreadWide    = spreadWide
+       }
 
+    /// 互換用の標準プリセット
+    static let standard = MBGGlowTuning()
+}
+/*
     /// Kit2 標準値（今までの数値）
     public static let standard = MBGGlowTuning(
         fillSubtle: 0.20,
@@ -51,3 +61,4 @@ public struct MBGGlowTuning: Sendable {
         baseBlurScale: 0.9    // いま haloGroup で使っている値
     )
 }
+*/
