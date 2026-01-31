ce8d775680b5bd72ea1bd3aa23c9de519a010689
Mou <29241499+Moumousan@users.noreply.github.com>
2026-02-01 03:14:51 +0900

    edit Package.swift macOS 15

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "ModernButtonKit2",
     platforms: [
-        .macOS(.v13),
+        .macOS(.v15),
         .iOS(.v18)
     ],
     products: [
